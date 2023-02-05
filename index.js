0acc77d4d76e99b77e46632104620e6e568a09d3
dipak-tomar <dipakkumartomar758@gmail.com>
2023-02-05 17:26:28 +0530

    fixed server error

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const connectDB = require("./server/config/db");
 
@@ -33,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.get("*", (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+      path.resolve(__dirname, "../", "client", "build", "index.html")
     )
   );
 } else {
