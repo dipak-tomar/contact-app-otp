c2fe9e9ea6e712c4b55653134e1cf04a5d98c1eb
dipak-tomar <dipakkumartomar758@gmail.com>
2023-02-05 18:40:00 +0530

    added env REACT_APP_SERVER

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-  baseURL: "https://contact-app-otp-api.onrender.com/api",
+  baseURL: `${process.env.REACT_APP_SERVER}/api`,
 });
 
 export default axios;
