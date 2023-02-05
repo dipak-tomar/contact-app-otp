835c9d2dc805c4f9520a8f4fa2e47db77b48a7a8
dipak-tomar <dipakkumartomar758@gmail.com>
2023-02-05 16:14:11 +0530

    added code for production build

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ app.get("/", (req, res) => {
 
 app.use("/api", router);
 
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./", "client", "build", "index.html"));
+});
+
 app.listen(port, () => {
   console.log("Listening on port", port);
 });
