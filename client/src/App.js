9697ee5c87451f69c6b0af3c61fcab34035dbeb4
dipak-tomar <dipakkumartomar758@gmail.com>
2023-06-07 22:57:46 +0530

    removed duplicate

--- client/src/App.js
+++ client/src/App.js
@@ -39,9 +39,6 @@ function App() {
                 <Link className="nav-link navbar-brand" to="/messages">
                   Message
                 </Link>
-                <Link className="nav-link navbar-brand" to="/messages">
-                  Message
-                </Link>
               </li>
             </ul>
           </div>
